ade8f773bd178f25c534d31261dadc545a393698
Oliver Farren <oliver.farren@gmail.com>
2020-04-30 22:35:34 +0100

    File Upload

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var usersRouter = require('./routes/users');
 var dishRouter  = require('./routes/dishRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var promoRouter  = require('./routes/promoRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 
@@ -59,7 +60,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/leaders', leaderRouter);
 app.use('/promotions', promoRouter);
-
+app.use('/imageUpload',uploadRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
